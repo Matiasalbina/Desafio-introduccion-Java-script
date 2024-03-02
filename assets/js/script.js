c818c46c92e4bfa0e15b1cc9eee655ecf4e6c60d
Matiasalbina <154567225+Matiasalbina@users.noreply.github.com>
2024-03-02 13:07:49 -0300

    Arreglo en funcion AumentarUnidades y se agrega const precioSpan

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,28 @@
 /* variable constante*/
 const precio = 400000;
-const cantidadElement = document.querySelector('.cantidad');
+const cantidadElement = document.querySelector(".cantidad");
 /*-----------------------------------------------------------------*/
 function calcularPrecioTotal(cantidad, precio) {
   const amount = Number(cantidad);
   const price = Number(precio);
   const total = price * amount;
 /*-----------------------------------------------------------------*/
-  return new Intl.NumberFormat("es-MX" ,{
-    style:"currency", currency: "CLP",}).format(total);
+  return new Intl.NumberFormat("es-MX" , {
+    style: "currency" ,
+    currency: "CLP" ,
+  }).format(total);
 }
 /*---------------------------------------------------------------- */
 
 function aumentarUnidades() {
-  const precioTotalElement = querySelector('.valor-total');
-  const cantidadValue = Number(document.querySelector('.cantidad').innerHTML);
+  const precioTotalElement = document.querySelector(".valor-total");
+
+  const cantidadElement = document.querySelector(".cantidad");
+  const cantidadValue = Number(document.querySelector(".cantidad").innerHTML);
   const nuevaCantidad = cantidadValue + 1;
+
   cantidadElement.innerHTML = nuevaCantidad;
+
   const valorTotal = calcularPrecioTotal(nuevaCantidad, precio);
   precioTotalElement.innerHTML = valorTotal;
   console.log(cantidadValue, valorTotal);
@@ -38,4 +44,5 @@ function disminuirUnidades() {
     }
 }
 
-/*Agregar funcion disminuir*/
+const precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
